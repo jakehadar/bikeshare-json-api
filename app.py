8ccf75e355d847d7bf81ab553569001925acb795
jake1025 <jake1025@gmail.com>
2019-09-16 01:33:52 -0400

    add status code and new endpoints for station info and status

--- app.py
+++ app.py
@@ -15,20 +15,79 @@ def systems():
 
 @app.route('/app/api/v1.0/system/<string:system_id>', methods=['GET'])
 def system_detail(system_id):
-    data = ds.get_system_by_id(system_id)
-    return jsonify(data)
+    result = ds.get_system_by_id(system_id)
+    if result is None:
+        return Response(status=404)
+
+    return jsonify(result)
 
 
 @app.route('/app/api/v1.0/system/<string:system_id>/feeds', methods=['GET'])
 def system_feeds(system_id):
     client = ds.instantiate_client(system_id)
+    if client is None:
+        return Response(status=404)
+
     return jsonify(client.feed_names)
 
 
 @app.route('/app/api/v1.0/system/<string:system_id>/feed/<feed_name>', methods=['GET'])
 def system_feed_detail(system_id, feed_name):
     client = ds.instantiate_client(system_id)
-    return jsonify(client.request_feed(feed_name))
+    if client is None:
+        return Response(status=404)
+
+    result = client.request_feed(feed_name)
+    if result is None:
+        return Response(status=404)
+
+    return jsonify(result)
+
+
+@app.route('/app/api/v1.0/system/<string:system_id>/stations', methods=['GET'])
+def system_stations(system_id):
+    client = ds.instantiate_client(system_id)
+    if client is None:
+        return Response(status=404)
+
+    result = client.request_feed('station_information').get('data').get('stations')
+    return jsonify(result)
+
+
+@app.route('/app/api/v1.0/system/<string:system_id>/station/<string:station_id>/information', methods=['GET'])
+def system_station_status(system_id, station_id):
+    client = ds.instantiate_client(system_id)
+    if client is None:
+        return Response(status=404)
+
+    feed = client.request_feed('station_information')
+    items = feed.get('data').get('stations')
+
+    try:
+        result = next(filter(lambda x: str(x.get('station_id')) == station_id, items))
+    except StopIteration:
+        return Response(status=404)
+
+    result.update({'last_updated': feed.get('last_updated'), 'ttl': feed.get('ttl')})
+    return jsonify(result)
+
+
+@app.route('/app/api/v1.0/system/<string:system_id>/station/<string:station_id>/status', methods=['GET'])
+def system_station_information(system_id, station_id):
+    client = ds.instantiate_client(system_id)
+    if client is None:
+        return Response(status=404)
+
+    feed = client.request_feed('station_status')
+    items = feed.get('data').get('stations')
+
+    try:
+        result = next(filter(lambda x: str(x.get('station_id')) == station_id, items))
+    except StopIteration:
+        return Response(status=404)
+
+    result.update({'last_updated': feed.get('last_updated'), 'ttl': feed.get('ttl')})
+    return jsonify(result)
 
 
 @app.route('/app/api/v1.0/system/<string:system_id>/station/<string:station_id>', methods=['GET'])
